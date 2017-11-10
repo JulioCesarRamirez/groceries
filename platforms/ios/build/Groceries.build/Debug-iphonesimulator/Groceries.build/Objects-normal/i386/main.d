45c27ea011de837171468daa10629b7d5d40f8d1
Juliocesarramirez <julio_marshal@hotmail.com>
2017-11-10 11:57:07 -0600

    added project

---
+++ platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/i386/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/julio/Documents/Nativescript/Groceries/platforms/ios/Groceries/Groceries-Prefix.pch \
+  /Users/julio/Documents/Nativescript/Groceries/platforms/ios/internal/main.m \
+  /Users/julio/Documents/Nativescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/julio/Documents/Nativescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/julio/Documents/Nativescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/julio/Documents/Nativescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/julio/Documents/Nativescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/julio/Documents/Nativescript/Groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/julio/Documents/Nativescript/Groceries/platforms/ios/internal/TNSDebugging.h \
+  /Users/julio/Documents/Nativescript/Groceries/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
