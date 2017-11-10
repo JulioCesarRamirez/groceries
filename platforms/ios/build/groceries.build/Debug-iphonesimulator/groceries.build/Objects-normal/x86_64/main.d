72ff1aaff10719e1d1c9fc69af1599884932a294
Juliocesarramirez <julio_marshal@hotmail.com>
2017-11-10 12:15:40 -0600

    compiled ios device

---
+++ platforms/ios/build/groceries.build/Debug-iphonesimulator/groceries.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/julio/Documents/Nativescript/NativeScriptApps/groceries/platforms/ios/groceries/groceries-Prefix.pch \
+  /Users/julio/Documents/Nativescript/NativeScriptApps/groceries/platforms/ios/internal/main.m \
+  /Users/julio/Documents/Nativescript/NativeScriptApps/groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/julio/Documents/Nativescript/NativeScriptApps/groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/julio/Documents/Nativescript/NativeScriptApps/groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/julio/Documents/Nativescript/NativeScriptApps/groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/julio/Documents/Nativescript/NativeScriptApps/groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/julio/Documents/Nativescript/NativeScriptApps/groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/julio/Documents/Nativescript/NativeScriptApps/groceries/platforms/ios/internal/TNSDebugging.h \
+  /Users/julio/Documents/Nativescript/NativeScriptApps/groceries/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
